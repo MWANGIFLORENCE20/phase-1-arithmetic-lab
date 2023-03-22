b1039aa9a61934486c3e544d439f201a8df40749
florence wangechi <florencewangechim@gmail.com>
2023-03-22 11:05:02 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
+let num1 = 2 ;
+let num2 = 31 ;
+let multiply = num1 * num2 ;
 
-// Write your code here
+console.log(multiply) ;
+
+
+const random = Math.ceil(Math.random()*100);
+
+
+let num3 = 7;
+let num4 = 18;
+const mod = num4 % num3;
+
+ 
+
+const max = Math.max(0,5,10,20);
+
+
